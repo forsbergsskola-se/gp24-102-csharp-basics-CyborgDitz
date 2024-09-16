ace1ebda0d96ac0b462c6fc453338f9f9c4c1844
CyborgDitz <sebban.lol@gmail.com>
2024-09-16 13:15:16 +0200

    Finisehd p4

--- P4Comments/Program.cs
+++ P4Comments/Program.cs
@@ -1,6 +1,6 @@
-﻿//Sebastian Ditz
-//2024-09-16
-//Sebban.lol@gmail.com
+﻿/*Sebastian Ditz
+2024-09-16
+Sebban.lol@gmail.com */
 
 Console.WriteLine("Hello, World!");
 Console.WriteLine("There are secrets within the comments");
