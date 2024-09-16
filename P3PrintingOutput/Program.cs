869ebae692cfcb78f31ded28cbedf9618d52c56e
CyborgDitz <sebban.lol@gmail.com>
2024-09-16 11:19:31 +0200

    Made Print Hello Thing

--- P3PrintingOutput/Program.cs
+++ P3PrintingOutput/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+//Goal: Output:Hello, World!
+Console.WriteLine("Welcome to 102 CSharp Basics!");
+//Output:Welcome to 102 CSharp Basics!
+Console.WriteLine("It's easy to print output...");
+//    Output:It's easy to print output...
+Console.WriteLine("...to the console!");
+//Output:...to the console!
+
+
