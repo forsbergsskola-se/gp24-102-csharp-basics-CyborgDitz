8e0b08152f9e68d7f0d554de4e753875bedc28a7
CyborgDitz <sebban.lol@gmail.com>
2024-09-16 14:17:38 +0200

    Bool is true awesome!

--- P6P6DataTypes/Program.cs
+++ P6P6DataTypes/Program.cs
@@ -1,7 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 string outputString;
 float decimalValue = 0.3f; //sets float value.
-
+bool isAwesome = true; // sets true or false value
 outputString = @"Hello world!"; //@ sets multible lines for string
 Console.WriteLine(outputString); //tring output
 Console.WriteLine(decimalValue); //float output
+Console.WriteLine(isAwesome); //bool output
