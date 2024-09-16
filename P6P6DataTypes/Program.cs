47f90354156601b8cc92f0f0674e113cb15b20bf
CyborgDitz <sebban.lol@gmail.com>
2024-09-16 14:15:12 +0200

    Set Float variable

--- P6P6DataTypes/Program.cs
+++ P6P6DataTypes/Program.cs
@@ -1,4 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 string outputString;
+float decimalValue = 0.3f; //sets float value.
+
 outputString = @"Hello world!"; //@ sets multible lines for string
-Console.WriteLine(outputString);
+Console.WriteLine(outputString); //tring output
+Console.WriteLine(decimalValue); //float output
