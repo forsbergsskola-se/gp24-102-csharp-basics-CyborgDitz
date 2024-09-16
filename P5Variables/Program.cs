6ae61ddbf06ed0497aa52c047cf264fdc0ed8997
CyborgDitz <sebban.lol@gmail.com>
2024-09-16 13:40:23 +0200

    Variable b = a

--- P5Variables/Program.cs
+++ P5Variables/Program.cs
@@ -4,9 +4,15 @@ string a;
 string b;
 //defines variables
 a = "Hello";
+
 // a variable is Hello
 Console.WriteLine(a);
 //prints Hello AKA a
 a = "world";
 Console.WriteLine(a);
-//Console.WriteLine(b);
+//Replaces old variable value, outprints world
+b = a;
+//B is current variable value of a, which is World
+Console.WriteLine(b);
+//prints world again.
+s
