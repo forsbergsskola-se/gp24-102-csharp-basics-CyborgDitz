cf2c3ff7a76511b437f68d11483339fa586e1de6
CyborgDitz <sebastian.ditz@student.forsbergsskola.se>
2024-09-17 09:17:42 +0200

    Install on Home PC did this

--- P5Variables/obj/Debug/net8.0/P5Variables.AssemblyInfo.cs
+++ P5Variables/obj/Debug/net8.0/P5Variables.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("P5Variables")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b619fb401b0836da089acf066a7fbf5dfccd416b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6eb68bd7e76752dcdd32bbe229ed8ecd56cb731e")]
 [assembly: System.Reflection.AssemblyProductAttribute("P5Variables")]
 [assembly: System.Reflection.AssemblyTitleAttribute("P5Variables")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
