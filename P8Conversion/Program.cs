c9930c9959e6470d1ca6fc4d8c93248ac65f7f36
CyborgDitz <sebastian.ditz@student.forsbergsskola.se>
2024-09-17 10:15:23 +0200

    P8 done

---
+++ P8Conversion/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine(@"A shady man with a deck of cards slips up to you. ´Hey, gimme a number. Any number between 1 and 10!´");
+
+string input = Console.ReadLine();
+float a =float.Parse(input); //a = float.Parse(input); //parse a float from string
+
+Console.WriteLine($"Output:"+ a);
+
+int b = Convert.ToInt32(a);
+Console.WriteLine($"Output:"+b);
+
