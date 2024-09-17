70f3d1f7687ee44d4487c91faf582585b8b30b70
CyborgDitz <sebastian.ditz@student.forsbergsskola.se>
2024-09-17 13:38:47 +0200

    Its bloated

--- P09Operators/Program.cs
+++ P09Operators/Program.cs
@@ -12,13 +12,17 @@ string input = Console.ReadLine();
 
 int math = Convert.ToInt32(input); //total input
 int a = math; //test variable
-int b = math % 60; //test variable
-int c = math % (60 * 60); //test variable
-float d = 
-Console.WriteLine(math + (seconds % 60 ) ); // shoulds print and divide stuff - it printed the remaining digit
+int b = math / 60; //test variable
+int c = math / (60*60); //test variable
+float d = math/ (60 * 60 * 24);
+//Console.WriteLine(math + (seconds % 60 ) ); // shoulds print and divide stuff - it printed the remaining digit
 //Test: WriteLine(math+b). - Problem. Printed value + modulo value.
 //Test: Formula update to set variable of seconds to minutes, as divided by 60
-
+Console.WriteLine(math);
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+Console.WriteLine(d);
 
 
 
