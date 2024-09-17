d6a09f14f6caa7616bac1f1eb59072fb8d6c37af
CyborgDitz <sebastian.ditz@student.forsbergsskola.se>
2024-09-17 09:18:05 +0200

    P6 added Output text and test code

--- P6P6DataTypes/Program.cs
+++ P6P6DataTypes/Program.cs
@@ -3,6 +3,11 @@ string outputString;
 float decimalValue = 0.3f; //sets float value.
 bool isAwesome = true; // sets true or false value
 outputString = @"Hello world!"; //@ sets multible lines for string
-Console.WriteLine(outputString); //tring output
-Console.WriteLine(decimalValue); //float output
-Console.WriteLine(isAwesome); //bool output
+Console.WriteLine($"Output:" + outputString); //tring output
+Console.WriteLine($"Output:" + decimalValue); //float output
+Console.WriteLine($"Output:" + isAwesome); //bool output
+
+//Learning from class
+char AdamChar = 'A';
+char BobChar = 'B';
+int doMath = AdamChar + BobChar;
