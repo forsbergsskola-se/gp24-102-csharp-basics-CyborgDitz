ef58e493a7a54f5da63bc4bfdca3a092dedf62df
CyborgDitz <sebastian.ditz@student.forsbergsskola.se>
2024-09-17 11:59:35 +0200

    Made some variables

--- P09Operators/Program.cs
+++ P09Operators/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// See https://aka.ms/new-console-template for more 
+
+using System.Net.Mime;
+
+Console.WriteLine("Gimme a couple seconds, or a bunch!. In. Here. NOW! TYYPPE THEM!!");
+string input = Console.ReadLine();
+
+
+float math = float.Parse(input);
+float seconds;
+float minutes;
+float hours;
+float days;
+
+//Console.WriteLine(z + z); // 60
