c8a06bf1f0709210c167e3d571f0e83a8dcca1bd
CyborgDitz <sebastian.ditz@student.forsbergsskola.se>
2024-09-17 13:07:47 +0200

    Tried Making modulo

--- P09Operators/Program.cs
+++ P09Operators/Program.cs
@@ -4,12 +4,20 @@ using System.Net.Mime;
 
 Console.WriteLine("Gimme a couple seconds, or a bunch!. In. Here. NOW! TYYPPE THEM!!");
 string input = Console.ReadLine();
-
+//"input-> displayed in seconds a
+// time formula = ( input / a / b / c / d) 
+//minutes b = ( input / 60 )
+//hours c = ( input / 60 / 60 ) -> b/=/c
+//days d= ( input / 60 / 60 / 24 )
 
 float math = float.Parse(input);
-float seconds;
+float seconds = math % 60;
+Console.WriteLine(seconds); // shoulds print and divide stuff
+
+
+
+/*float seconds;
 float minutes;
 float hours;
-float days;
+float days; */
 
-//Console.WriteLine(z + z); // 60
