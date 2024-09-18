2505225ea739cc5ec10dde302775a7c8fa6cb55e
CyborgDitz <sebastian.ditz@student.forsbergsskola.se>
2024-09-18 09:35:05 +0200

    Renamed inputs to 1 and 2 etc, and made minutes to seconds program

--- P09Operators/Program.cs
+++ P09Operators/Program.cs
@@ -2,20 +2,26 @@
 
 // im confused this week lmao - Seb
 //Console.WriteLine("Gimme a couple seconds, or a bunch!. In. Here. NOW! TYYPPE THEM!!");
-Console.WriteLine("Gimme a number!");
-string input = Console.ReadLine();
+Console.WriteLine("Gimme a kilometer or two!!");
+string input1 = Console.ReadLine();
 
 //exercise 0.1 speedconverter
 Console.WriteLine("exercise 0.1 speedconverter");
-int KH = Convert.ToInt32(input);
+int KH = Convert.ToInt32(input1);
 Console.WriteLine($"Beep boop. I think this is Kilometers per hour... "+ KH);
-// KH to MS is roughly KH / 3.6
-double KHToMS = KH / 3.6;
+
+double KHToMS = KH / 3.6; // KH to MS is roughly KH / 3.6
 Console.WriteLine($"Beep boop. I think this is Meters per Second... "+ KHToMS);
 
-//
-Console.WriteLine("exercise 0.2 speedconverter");
+//exercise 0.2 MinutesToSeconds
+Console.WriteLine("exercise 0.2 MinutesToSeconds");
+Console.WriteLine("Gimme a few minutes!");
+string input2 = Console.ReadLine();
+int Min = Convert.ToInt32(input2);
 
+int Sec = Min * 60;
+Console.WriteLine("This is seconds now! " + Sec + " seconds");
+    
 /*EVERYTHING BELOW THIS LINE IS SUBJECT TO TRASH 
 
 //"input-> displayed in seconds a
