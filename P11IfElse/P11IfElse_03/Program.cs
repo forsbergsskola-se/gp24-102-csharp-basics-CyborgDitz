3fb6edd246e460f895e7ff18bb6b0183eaf61219
CyborgDitz <sebastian.ditz@student.forsbergsskola.se>
2024-09-19 12:03:33 +0200

    Making switch in 03 for testing

---
+++ P11IfElse/P11IfElse_03/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Type a singel character!");
+string input = Console.ReadLine();
+
+switch (input)
+{
+    case "a":
+        break;
+    case "e":
+        break;
+    case "i":
+        break;
+    case "o":
+        break;
+    case "u":
+        break;
+    case "y":
+        break;
+    case "å":
+        break;
+    case "ä":
+        break;
+    case "ö":
+        break;
+}
+
