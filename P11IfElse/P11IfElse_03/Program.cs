23088c11398d5871a30cce9ee9e1ef8215e9a293
CyborgDitz <sebastian.ditz@student.forsbergsskola.se>
2024-09-19 13:51:27 +0200

    Made 11.03 spaghetti

--- P11IfElse/P11IfElse_03/Program.cs
+++ P11IfElse/P11IfElse_03/Program.cs
@@ -1,26 +1,62 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Type a singel character!");
 string input = Console.ReadLine();
+// string isVowel = input;
 
+//clarifying that i tested switches for the first time
 switch (input)
-{
-    case "a":
-        break;
+{ //checks wowel
+    case "a": 
     case "e":
-        break;
     case "i":
-        break;
     case "o":
-        break;
-    case "u":
-        break;
-    case "y":
-        break;
-    case "å":
-        break;
-    case "ä":
-        break;
+    case "u": 
+    case "y": 
+    case "å": 
+    case "ä": 
     case "ö":
+        Console.WriteLine("THAS A VOWEL MATE!");
         break;
-}
+    //checks consonant
+    case "b":
+    case "c":
+    case "d":
+    case "f":
+    case "g":    
+    case "h":
+    case "j":
+    case "k":
+    case "l":
+    case "m":
+    case "n":
+    case "p":
+    case "q":
+    case "r":
+    case "s":
+    case "t":
+    case "v":
+    case "w":
+    case "x":
+    case "z":
+        Console.WriteLine("THAS A CONSONONTOTONONR MATE!");
+        break;
+    
+    //checks numbero
+    case "1":
+    case "2":
+    case "3":
+    case "4":
+    case "5":
+    case "6":
+    case "7":
+    case "8":
+    case "9":
+    case "0":
+        
+        Console.WriteLine("THAS A NUMBRE BOZO!");
+        break;
+    
+             }  
+
+
 
