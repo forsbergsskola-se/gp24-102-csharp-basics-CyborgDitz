c55eb5e91d87c29e9869182f4113d63720a22785
CyborgDitz <sebastian.ditz@student.forsbergsskola.se>
2024-09-19 10:29:01 +0200

    P11 part 1

---
+++ P11IfElse/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("What's your age?");
+var age = Convert.ToInt32(Console.ReadLine());
+if (age >= 18)
+{
+    Console.WriteLine("You are an adult!");
+}
+
+if (age < 18 && age >=13)
+{
+    Console.WriteLine("You are a teen!");
+}
+
+if (age <= 12 && age >0)
+{
+    Console.WriteLine("You are a child!");
+}
+if (age <=0) {
+    Console.WriteLine("What... What are you?!");
+}
