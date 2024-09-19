419c8c4cd85e680ddaa2a8d142fe2d69848ecf83
CyborgDitz <sebastian.ditz@student.forsbergsskola.se>
2024-09-19 14:01:52 +0200

    Made a calculator

---
+++ P11IfElse/P11IfElse_04/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Type something then do something then type something");
+int input1 = int.Parse(Console.ReadLine());
+string Minput = Console.ReadLine();
+int input2 = int.Parse(Console.ReadLine());
+
+switch (Minput)
+{
+    case "+":
+        Console.WriteLine(input1 + input2);
+        break;
+    case "-":
+        Console.WriteLine(input1 - input2);
+        break;
+    case "/":
+        Console.WriteLine(input1 / input2);
+        break;
+    case "*":
+        Console.WriteLine(input1 * input2);
+        break;
+}
