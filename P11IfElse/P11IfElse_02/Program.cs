4c0fa53d7aa21916e2645223c7e185bcb70ec917
CyborgDitz <sebastian.ditz@student.forsbergsskola.se>
2024-09-19 11:45:00 +0200

    Making sort lowest in p11_2

---
+++ P11IfElse/P11IfElse_02/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("gimme a number");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+
+if (a < b && a < c)
+{
+    Console.WriteLine("lowest is "+ 
+                      a);
+}
+else if (b < a && b < c)
+{
+    Console.WriteLine("Lowest is " +b);
+}
+else
+{
+    Console.WriteLine("lowest is " + c);
+}
+
