bc4e40addc36c23e92fbd38adfb9d3805e49e1d0
CyborgDitz <sebban.lol@gmail.com>
2024-09-23 13:59:38 +0200

    Go to p13.2

--- P13GoTo/P13_2Triangle/Program.cs
+++ P13GoTo/P13_2Triangle/Program.cs
@@ -1,4 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("How biiiig triangle eh?");
 var tri = int.Parse(Console.ReadLine());
-if (tri) {}
+int i = 0;
+repeat:
+if (i < tri) 
+{
+    bonus:
+    Console.Write("#");
+    i++;
+    goto repeat;
+}
