b1d18179ccb3212e3df2a14a28c4f0d7318880fb
CyborgDitz <sebban.lol@gmail.com>
2024-09-23 08:56:29 +0200

    FInished p10 bool

--- P10Boolean/Program.cs
+++ P10Boolean/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Age please?");
+var age = Convert.ToInt32(Console.ReadLine());
+bool isChild = 12>= age; // check if age below 12 bool
+bool isTeen = 13 <= age && age <= 19; // check if age between 13 and 19 bool
+bool isAdult = 20 <= age; // check if age is above 19
+Console.WriteLine($"You are a Child: {isChild}");
+Console.WriteLine($"You are a Teenager: {isTeen}");
+Console.WriteLine($"You are an Adult: {isAdult}");
