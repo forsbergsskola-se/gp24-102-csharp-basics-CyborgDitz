9ceb5f17fbfc96bef2cdda5c23b9c63e3ba56b9e
CyborgDitz <sebban.lol@gmail.com>
2024-09-23 09:45:18 +0200

    Finished P12_4 and updated marginals of seed max value

--- P12Random/P12_2Random_Password/Program.cs
+++ P12Random/P12_2Random_Password/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Random random = new Random();
-string randomPassword = random.Next(100000,999999).ToString();
+string randomPassword = random.Next(100000,1000000).ToString();
 //^ sets seed to 6 digits
 Console.WriteLine("New Password: " + randomPassword);
