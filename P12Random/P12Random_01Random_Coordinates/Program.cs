6dec9c6dbeb742854f2bc2cc1bde60d5a0d1781c
CyborgDitz <sebastian.ditz@student.forsbergsskola.se>
2024-09-26 07:35:11 +0200

    Finished p13

--- P12Random/P12Random_01Random_Coordinates/Program.cs
+++ P12Random/P12Random_01Random_Coordinates/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Random random = new Random();
-int x = random.Next(1, 101);
-int y = random.Next(1, 101);
+int x = random.Next(1, 100);
+int y = random.Next(1, 100);
 Console.WriteLine("x:" + x + " y:" + y);
---
+++ P13GoTo/P13GoToMain/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("I picked a number (1-100), guess it");
+Random random = new Random();
+int x = random.Next(1, 100);
+
+string wrong1 = "My answer is Greater!";
+string wrong2 = "My answer is Lower!";
+string answer = "Thats right!";
+
+goback:
+int guess = int.Parse(Console.ReadLine());
+
+if (guess == x) {
+    Console.WriteLine(answer);
+}
+else if (guess < x)
+{ Console.WriteLine(wrong1); }
+else if (guess > x)
+{ Console.WriteLine(wrong2);
+    
+}
+goto goback;
+// output--;
+// if (output > 0)
+// {
+//     goto goback;
+// }
