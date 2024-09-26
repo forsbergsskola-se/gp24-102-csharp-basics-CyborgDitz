e86e5d6242cf54d869fccdfd26d020d7f61d6386
CyborgDitz <sebastian.ditz@student.forsbergsskola.se>
2024-09-26 06:57:24 +0200

    finished p13_3

--- P13GoTo/P13GoTo_3Pattern/Program.cs
+++ P13GoTo/P13GoTo_3Pattern/Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Enter a size");
+
+string patOne = "-#-#-#-#-#";
+string patTwo = "#-#-#-#-#-";
+
+int output = int.Parse(Console.ReadLine());
+
+goback:
+
+if (output % 2 ==0) {
+    Console.WriteLine(patOne);
+    }
+    else { Console.WriteLine(patTwo); }
+
+    output--;
+    if (output > 0)
+    {
+        goto goback;
+    }
+     
+     
